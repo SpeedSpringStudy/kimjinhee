d8c75ef6dc004e6093c3a27ffa5fab38828d9e14
kimzini <kimjinhee.dev@gmail.com>
2025-07-11 00:46:12 +0900

    chore: 스프링 초기 세팅

---
+++ src/main/java/backend/speedspringstudy/SpeedspringstudyApplication.java
@@ -0,0 +1,13 @@
+package backend.speedspringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedspringstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedspringstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/speedspringstudy/SpeedspringstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.speedspringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedspringstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
