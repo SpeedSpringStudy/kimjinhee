b4db85395c9f5a4dc74c761953c9a86bda811f97
kimzini <kimjinhee.dev@gmail.com>
2025-07-13 00:42:17 +0900

    feat: week1-2 구현

---
+++ src/main/java/backend/speedspringstudy/controller/ViewController.java
@@ -0,0 +1,24 @@
+package backend.speedspringstudy.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping(value = "/products", produces = MediaType.TEXT_HTML_VALUE)
+    public String productsPage() {
+        return "products";
+    }
+
+    @GetMapping(value = "/products/create", produces = MediaType.TEXT_HTML_VALUE)
+    public String productCreatePage() {
+        return "product_form";
+    }
+
+    @GetMapping(value = "/products/edit", produces = MediaType.TEXT_HTML_VALUE)
+    public String productEditPage() {
+        return "product_edit_form";
+    }
+}
