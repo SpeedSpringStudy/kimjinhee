3b536902ae990222eef86878565ce9f2dfbae2e9
kimzini <kimjinhee.dev@gmail.com>
2025-07-22 22:04:52 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/backend/speedspringstudy/auth/service/AuthService.java
+++ src/main/java/backend/speedspringstudy/auth/service/AuthService.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
